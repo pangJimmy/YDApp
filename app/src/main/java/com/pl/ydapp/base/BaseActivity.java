45af2c06247edc9534f1c0d1b16cf15eb60db366
pangshanwei <pang@qq.com>
2019-05-17 00:59:50 +0800

    初次提交，UI图标加入

---
+++ app/src/main/java/com/pl/ydapp/base/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.pl.ydapp.base;
+
+import android.app.Activity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+/**
+ * 基础类，提供公用接口，设置标题，弹窗，提示
+ */
+public class BaseActivity extends Activity {
+
+    public Toolbar toolbar ;
+    private TextView tvTitle ;
+
+}
